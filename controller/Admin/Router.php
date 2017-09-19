53242976b4e92d529ec5c4e549ed090bb8cbd159
dnllromao <dnllromao@gmail.com>
2017-09-19 16:56:09 +0200

    restart

---
+++ controller/Admin/Router.php
@@ -0,0 +1,34 @@
+<?php 
+
+namespace Controller\Admin;
+
+class Router {
+	
+	public function index() {
+		ob_start();
+		require 'views/parts/home.php';
+		$content = ob_get_clean();
+		require 'views/template.php';
+	}
+
+	public function page($slug) {
+		$title = ucfirst(str_replace('-', ' ', $slug));
+		// $post = \Model\Model::getInstance()->find($slug);
+		//var_dump($post);
+
+		
+		switch ($slug) {
+		 	case 'interview':
+		 		ob_start();
+		 		require "views/parts/$slug.php";
+		 		$content = ob_get_clean();
+		 		require 'views/template.php';
+		 		break;
+		 	
+		 	default:
+		 		require 'views/default.php';
+		 		break;
+		} 
+		
+	}
+}
